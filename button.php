ab393273aa58f9ead5ee74246a9e14007f30d957
zed <20442970+proamg9@users.noreply.github.com>
2018-10-04 03:31:30 +0100

    first commit

---
+++ button.php
@@ -0,0 +1,9 @@
+<?php 
+	
+	
+
+	echo "<h5>Thank you for registering.  Somebody will contact you soon!!</h5>";
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+	
+	<title>WBECS</title>
+	<meta name="viewport" content="width= device-width">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<div class="container">
+
+			<div id="parent">
+				
+				<img class="image2" src="img/bann.png">
+			
+				
+				<img class="image1" src="img/head.png" alt="header"> 
+   				
+				
+  				<img class="image3" src="img/commwith.png" alt="Main text">
+
+  				<a href="button.php">
+			<img src="img/bt1a.png" style="width: 140px;height:25px;position: absolute;    background-color: transparent;border:none;top: 25px;left: 1187px;z-index: 10;" title="Pre-Register" alt="Register"></a>
+  				
+  				<a href="button.php">
+			<img src="img/bt2a.png" style="width: 258px;height: 32px;position: absolute;background-color: transparent;border:none;top: 1772px;left: 546px;z-index: 10;" title="Click to Register" alt="Register"></a>
+
+			</div>
+
+				
+
+
+		<div class="firstbox">
+			<img class="image5" src="img/rec1.png">
+			<img class="image4" src="img/countd.png" alt="Countdown to event">
+			<div id="t1">
+				<p> Used by Executive Coaches in over 32 countries,
+receive a 5-10x return on your investment in this program from your client fees and command  the potential of $1500 to $5500 per month, per client. </p>
+				
+
+
+			</div>
+
+		</div>
+
+		<div class="whitebox">
+			<img class="image6" src="img/whiteBK.png">
+			<img class="image7" src="img/elip1.png">
+			<div id="t2">
+				<p>
+			A comprehensive collection
+of business solutions in the palm
+of your hand</p>
+
+			</div>
+
+
+		<div>
+		<img class="image8" src="img/text2.png">
+		<img class="image9" src="img/block2.png">
+		<a href="button.php">
+			<img src="img/bt2a.png" style="width: 258px;height: 32px;position: absolute;background-color: transparent;border:none;top: 351px;left: 335px;z-index: 10;" title="Click to Register" alt="Register"></a>
+		<img class="image10" src="img/testim.png">
+		<img class="image11" src="img/toolandcountd.png">
+		</div>
+		
+		
+
+
+		</div>
+
+		<div class="footer">
+			<img class="image12" src="img/about.png">
+
+			<a href="button.php">
+			<img src="img/bt2a.png" style="width: 258px;height: 32px;position: absolute;background-color: transparent;border:none;top: 4209px;left: 569px;z-index: 10;" title="Click to Register" alt="Register"></a>
+		</div>
+		
+</div>
+
+	
+		
+		
+
+
+</body>
+</html>
